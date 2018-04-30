392a0f44513ae4594946657c60ad3050a3f274cd
Pawel Bus <pawelbus.dev@outlook.com>
2018-04-30 20:55:00 +0200

    Fix bug with change slide on start in changeSlide func

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ $(function () {
   function changeSlide() {
     carouselList.animate({
       left: '-400px'
-    }, 500, moveFirstSlide());
+    }, 500, moveFirstSlide);
   }
 
   function moveFirstSlide() {
     var firstItem = carouselList.find('li:first'),
-      lastItem = carouselList.find('li:last');
+        lastItem = carouselList.find('li:last');
 
     lastItem.after(firstItem);
     carouselList.css({
