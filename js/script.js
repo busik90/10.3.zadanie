3e87aee6ee5489f478279c9830ac541fb9b3fa2b
Pawel Bus <pawelbus.dev@outlook.com>
2018-05-01 19:15:24 +0200

    Make functionality to arrows

--- js/script.js
+++ js/script.js
@@ -1,21 +1,48 @@
 $(function () {
-  var carouselList = $('#carousel ul');
+  var carouselList = $('#carousel ul'),
+      interval;
 
-  var interval = setInterval(changeSlide, 3000);
-
-  function changeSlide() {
-    carouselList.animate({
-      left: '-400px'
-    }, 500, moveFirstSlide);
+  $('#js-btn-left').click(function() { changeSlide('left') });
+  $('#js-btn-right').click(function() { changeSlide('right') });
+      
+  function autoMoveSlide() {
+    interval = setTimeout(function() { changeSlide('left') }, 3000);
   }
 
-  function moveFirstSlide() {
+  autoMoveSlide();
+
+  function changeSlide(direction) {
+    console.log('changeSlide: ' + direction);
+    switch(direction) {
+      case 'left':       
+        carouselList.animate({ left: '-800px' }, 500, function() { moveSlide(direction) });
+        break;
+      case 'right':    
+        carouselList.animate({ left: '0px' }, 500, function() { moveSlide(direction) });
+        break;
+    }
+  }
+
+  function moveSlide(direction) {
+    console.log('moveSlide: ' + direction);
     var firstItem = carouselList.find('li:first'),
         lastItem = carouselList.find('li:last');
 
-    lastItem.after(firstItem);
-    carouselList.css({
-      left: 0
-    });
+    switch(direction) {
+      case 'left':
+        lastItem.after(firstItem);
+        carouselList.css({
+          left: '-400px'
+        });
+        break;
+      case 'right':
+        firstItem.before(lastItem);
+        carouselList.css({
+          left: '-400px'
+        });
+        break;
+    }
+    clearTimeout(interval);
+    autoMoveSlide();
   }
 });
