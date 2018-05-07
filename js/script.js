44204fb695708589965bf4d9c05893d36b4a6f3c
Pawel Bus <pawelbus.dev@outlook.com>
2018-05-07 21:53:19 +0200

    Rebuild script

--- js/script.js
+++ js/script.js
@@ -1,66 +1,131 @@
 $(function () {
-  var carouselList = $('#js-carousel-content'),
-      carouselItems = carouselList.find('li'),
+	var carouselList = $('#js-carousel-content'),
+			carouselItems = carouselList.find('li'),
 
-      sliderNav = $('#js-carousel-nav'),
-      sliderNavItems = sliderNav.find('li'),
+			sliderNav = $('#js-carousel-nav'),
+			sliderNavItems = sliderNav.find('li'),
 
-      interval,
-      carouselPos = 0,
-      navPos = 0;
-      
-  // events
-  $('#js-btn-left').click(function() { setCarouselPosition('preview') });
-  $('#js-btn-right').click(function() { setCarouselPosition('next') });
-  sliderNavItems.click(function() { setCarouselPosition( $(this).index() ) });
-      
-  function autoMoveSlide() {
-    interval = setTimeout(function() { setCarouselPosition('next') }, 3000);
-  }
+			timer,
+			steps,
+			shift,
+			currentPos = 0,
+			direction;
+			
+	// events
+	$('#js-btn-left').click(function() { setNewPos('preview') });
+	$('#js-btn-right').click(function() { setNewPos('next') });
+	sliderNavItems.click(function() { setNewPos( $(this).index() ) });
+			
+	function startCarousel() {
+		timer = setTimeout(function() { setNewPos("next") }, 3000);
+	}
 
-  autoMoveSlide();
+	startCarousel();
 
-  function setCarouselPosition(moveTo) {
+	function setNewPos(newPos) {
+		if (newPos == 'next') {
+			newPos = currentPos;
+			newPos++;
+		} else if (newPos == 'preview') {
+			newPos = currentPos;
+			newPos--;
+		}
 
-    if(moveTo == 'next') {
-      carouselPos = carouselPos + 400;
-      navPos++;
-      
-    } else if(moveTo == 'preview') {
-      carouselPos = carouselPos - 400;
-      navPos--;
+		if (newPos > 4) {
+			newPos = 0;
+		} else if (newPos < 0) {
+			newPos = 4;
+		}
 
-    } else {
-      carouselPos = 400*moveTo;
-      navPos = moveTo;
+		steps = currentPos - newPos;
+		steps = Math.abs(steps);
+
+		// set direction
+		if (steps == sliderNavItems.length - 1) {
+			steps = 1;
+			switch (currentPos) {
+				case 0:
+					direction = 'left';
+					break;
+				case 4:
+					direction = 'right';
+					break;
+			}
+		} else if (newPos < currentPos) {
+				direction = 'left';
+		} else if (newPos > currentPos) {
+				direction = 'right';
+		}
+
+		console.log('-------------------');
+		console.log('currentPos: ' + currentPos);
+		console.log('newPos: ' + newPos);
+		console.log('steps: ' + steps);
+		console.log('direction: ' + direction);
+		console.log('-------------------');
+
+		currentPos = newPos;
+
+		sliderNavItems.removeClass('active');
+		sliderNavItems.eq(currentPos).addClass('active');
+
+		changeSlide();
+	}
+
+	function changeSlide() {
+		shiftDuration = 500 / steps * 2;
+
+		switch(direction) {
+			case 'left':
+				for (i=1; i <= steps; i++) {
+					carouselList.animate({ left: '0px' }, shiftDuration, function() { moveSlide(direction) });
+				}
+        break;
+      case 'right':    
+				for (i=1; i <= steps; i++) {
+					carouselList.animate({ left: '-800px' }, shiftDuration, function() { moveSlide(direction) });
+				}
+				break;
     }
+		
+	  clearTimeout(timer);
+	  startCarousel();
+	}
 
-    if (carouselPos == 2000) {
-      carouselPos = 0;
-      navPos = 0;
-    } else if (carouselPos == -400) {
-      carouselPos = 1600;
-      navPos = 4;
+	function moveSlide(direction) {
+	  var firstItem = carouselList.find('li:first'),
+	      lastItem = carouselList.find('li:last');
+		
+		switch(direction) {
+			case 'left':
+        firstItem.before(lastItem);
+        carouselList.css({
+          left: '-400px'
+        });
+        break;
+      case 'right':
+        lastItem.after(firstItem);
+        carouselList.css({
+          left: '-400px'
+        });
+        break;
     }
+	}
 
-    sliderNavItems.removeClass('active');
-    sliderNavItems.eq(navPos).addClass('active');
+	// stop carousel when hover
+	$('#carousel').hover(function() { carouselMove('stop') }, function() { carouselMove('start') });
 
-    changeSlide();
-  }
+	function carouselMove(move) {
+		switch (move) {
+			case 'stop':
+				clearTimeout(timer);
+				break;
+			case 'start':
+				startCarousel();
+				break;
+		}
 
-  function changeSlide() {
-    carouselList.animate({ left: -carouselPos }, 500);
-
-    clearTimeout(interval);
-    autoMoveSlide();
-  }
-
-  // stop carousel when hover
-  $('#carousel').hover(function() {
-    clearTimeout(interval);    
-  }, function() {
-    autoMoveSlide();
-  });
+		console.log('carousel: ' + move);
+	}
 
 });
